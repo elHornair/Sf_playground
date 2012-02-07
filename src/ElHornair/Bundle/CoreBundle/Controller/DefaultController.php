cc28ca418f47d1778874c54a11bf3700ea723905
Alain Horner <alain.horner@liip.ch>
2012-02-07 11:01:09 +0100

    init corebundle

---
+++ src/ElHornair/Bundle/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ElHornair\Bundle\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+
+    public function indexAction()
+    {
+        return $this->render('ElHornairCoreBundle:Default:index.html.twig');
+    }
+}
---
+++ src/ElHornair/Bundle/CoreBundle/ElHornairCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ElHornair\Bundle\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ElHornairCoreBundle extends Bundle
+{
+}
